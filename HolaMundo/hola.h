75635e2162a04f083e0196dbae3440e41b9f82cf
LuisEnriqueCabelloBlanco <luisecab@ucm.es>
2024-09-22 00:22:20 +0200

    Primeros pasos en Cmake
    se he llegado hasta el paso 2 apartado 1 del tutorial de cmake

---
+++ HolaMundo/hola.h
@@ -0,0 +1,3 @@
+// the configured options and settings for Tutorial
+#define HolaMundo_VERSION_MAJOR 1
+#define HolaMundo_VERSION_MINOR 0
---
+++ HolaMundo/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "hola.h"
+#include "MathFunctions.h"
+
+int main(){
+
+    auto a = 3;
+    std::cout << "Hola Mundo"<<"Version "<<HolaMundo_VERSION_MAJOR<< "."<<HolaMundo_VERSION_MINOR<<mathfunctions::sqrt(40)<< std::endl;
+    return 0;
+}
